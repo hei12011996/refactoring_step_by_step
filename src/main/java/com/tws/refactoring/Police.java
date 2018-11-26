5ad7b5c1422dbf3797124652f8ecc1e228ce086d
Antony Chan <antony.chan@oocl.com>
2018-11-26 15:54:03 +0800

    add static to constant

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -1,7 +1,7 @@
 package com.tws.refactoring;
 
 public class Police {
-    private final int legalAdultAge = 18;
+    private final static int legalAdultAge = 18;
 
     public boolean checkIfDriverIsAdult(Driver driver) {
         return driver.getAge() >= legalAdultAge;
