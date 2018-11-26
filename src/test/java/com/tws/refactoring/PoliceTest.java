def7f08332e9b463eaf73e9293a5f1c3388b16ed
Antony Chan <antony.chan@oocl.com>
2018-11-26 14:59:36 +0800

    add unit test for Police

---
+++ src/test/java/com/tws/refactoring/PoliceTest.java
@@ -0,0 +1,21 @@
+package com.tws.refactoring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PoliceTest {
+    private static Police police = new Police();
+
+    @Test
+    public void should_return_true_when_police_check_an_adult_driver(){
+        Driver adultDriver = new Driver(18);
+        assertTrue(police.checkDriver(adultDriver));
+    }
+
+    @Test
+    public void should_return_false_when_police_check_an_teenager_driver(){
+        Driver adultDriver = new Driver(5);
+        assertFalse(police.checkDriver(adultDriver));
+    }
+}
